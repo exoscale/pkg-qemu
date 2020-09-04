b9c0791db6d0066754e6129c7ef9082560cc8e43
Alessandro Ratti <alessandro.ratti@exoscale.ch>
2020-09-04 14:38:17 +0200

    Add qemu_4.2 for Ubuntu Focal
    This is a port from ubuntu/focal-updates with the addition of custom
    patch to get rid of unwanted/unmaintained ROMs
    
    [ch17899]

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-base=http://wiki.qemu-project.org/download
+base=https://download.qemu.org/
 comp=.xz
 
 dir() {
@@ -18,7 +18,7 @@ clean_dfsg() {
 
 rm -vf pc-bios/bios.bin		# roms/seabios/
 rm -vf pc-bios/bios-256k.bin	# roms/seabios/
-rm -vf pc-bios/*-dsdt.aml	# roms/seabios/
+rm -vf pc-bios/hppa-firmware.img	# roms/seabios-hppa/
 rm -vf pc-bios/ppc_rom.bin	# comes from openhackware
 rm -vf pc-bios/sgabios.bin	# roms/sgabios/
 rm -vf pc-bios/slof.bin		# roms/SLOF/
@@ -27,6 +27,7 @@ rm -vf pc-bios/vgabios*.bin	# roms/vgabios/
 rm -vf pc-bios/pxe-*.rom	# roms/ipxe/
 rm -vf pc-bios/efi-*.rom	# roms/ipxe/
 rm -vf pc-bios/bamboo.dtb	# pc-bios/bamboo.dts
+rm -vf pc-bios/canyonlands.dtb
 rm -vf pc-bios/openbios-*	# roms/openbios/
 rm -vf pc-bios/palcode-clipper	# roms/qemu-palcode/ alpha palcode
 rm -vf pc-bios/s390-ccw.img	# pc-bios/s390-ccw/
@@ -37,24 +38,31 @@ rm -vf pc-bios/linuxboot_dma.bin
 rm -vf pc-bios/multiboot.bin
 rm -vf pc-bios/skiboot.lid	# roms/skiboot/
 rm -vf pc-bios/u-boot.e500	# roms/u-boot/
+rm -vf pc-bios/u-boot-sam460-20100605.bin	# roms/u-boot-sam460ex/
 rm -vf pc-bios/QEMU,*.bin	# roms/openbios/
+rm -vf pc-bios/qemu_vga.ndrv	# roms/QemuMacDrivers/
 
 # remove other software (git submodules)
 dir roms/ipxe
 dir roms/openbios
 dir roms/openhackware
-dir roms/qemu-palcode
+#dir roms/qemu-palcode
 dir roms/seabios
-dir roms/sgabios
+dir roms/seabios-hppa
+#dir roms/sgabios
 dir roms/SLOF
+#rm -vf roms/SLOF/board-js2x/rtas/i2c_bmc.oco
+#rm -vf roms/SLOF/board-js2x/rtas/ipmi_oem.oco
+#rm -vf roms/SLOF/clients/takeover/takeover.oco
+#rm -vf roms/SLOF/lib/libipmi/libipmi.oco
 dir roms/skiboot
 dir roms/vgabios
 dir roms/u-boot
 rm -vf roms/u-boot.tar.*
+dir roms/u-boot-sam460ex
 dir roms/QemuMacDrivers
 
 dir dtc
-#dir pixman
 dir capstone
 
 find scripts -name '*.pyc' -print -delete
@@ -70,7 +78,7 @@ case "$#$1" in
     echo "apparently not a qemu source dir" >&2; exit 1
     ;;
 
-  1[012].*) ;;
+  1[01234].*) ;;
 
   *)
     echo "unknown arguments.  Should be either 'dfsg' or a version number" >&2
@@ -84,7 +92,7 @@ case "$upstream" in
    *~rc*) upstream=$(echo "$upstream" | sed 's/~rc/-rc/') ;;
 esac
 case "$upstream" in
-   2.[0-9] | 2.[0-9][!0-9.]* ) # add .0 to a version number
+   [234].[0-9] | [234].[0-9][!0-9.]* ) # add .0 to a version number
      upstream=$(echo "$upstream" | sed 's/^.\../&.0/') ;;
 esac
 
